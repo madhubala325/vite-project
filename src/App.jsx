759b48a6a71cbdac4dd7bd375700a1b9a885f149
madhu <madhubala325@gmail.com>
2026-09-24 22:22:40 +0530

    something add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Product from './Product'
 
 function App() {
   return (
     <div>
       <h1>Hello this is reactjs trial</h1>
+      <Routes>
+        <Route path='/product' element={<Product/>}/>
+      </Routes>
     </div>
   )
 }
